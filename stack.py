88790fe45d2faef1f2e0d0f726e5fdb83defc6e5
HarirajG <68351605+HarirajG@users.noreply.github.com>
2020-09-14 23:00:33 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self,char):
+        self.char = char
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        print("_"*15,"Data printing","_"*15)
+        temp = self.head
+        stt = ""
+        while(temp != None):
+            stt += '\n'+str(temp.char)
+            temp = temp.next
+        return stt
+    
+    def insertNode(self):
+        if self.head == None:
+            self.head = Node(input("Enter the Head data:"))
+        else:
+            temp = self.head
+            while temp.next != None:
+                temp = temp.next
+            temp.next = Node(input("Enter the data:"))
+
+    def deleteNode(self):
+        if self.head == None:
+            print("List is Empty")
+        else:
+            temp = self.head
+            self.head = self.head.next
+            print(temp.char,"is deleted")
+            del temp
+            
+#implemention of Stack using linked list
+if __name__ == '__main__':
+
+    #execution loop
+    list1 = LinkedList()
+    while True:
+        option = int(input("*"*30+"\nPress 1 for insert\nPress 2 for del \nPress 3 for print\nPress any key for Exit\nEnter your option:"))
+        print("*"*30)
+        if option == 1:
+            list1.insertNode()
+        elif option == 2:
+            list1.deleteNode()
+        elif option == 3:
+            print(list1)
+        else:
+            break
+
+print("Thankyou")
